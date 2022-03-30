ed1825ac2a26a6117e232a925442471ecad43313
GopalLohar <gopalloharnew@gmail.com>
2022-03-30 13:33:47 +0530

    search

--- projects.js
+++ projects.js
@@ -1,1 +1,14 @@
 console.log("Visit my Projects");
+
+const searchInput = document.querySelector(".search-input");
+const crossButton = document.querySelector(".cross-button");
+
+searchInput.addEventListener('input', ()=> {
+    q = searchInput.value.trim().toLowerCase();
+    console.log(q);
+})
+
+crossButton.addEventListener('click', ()=>{
+    searchInput.value = "";
+    searchInput.focus();
+})
